2d08dfab74c9c845765d3212f69b0aedc597a264
Nardos Tsige <tsigeferejanardos@gmail.com>
2026-03-18 10:06:55 +0300

    Optimize missingNumber method for better performance
    Refactored missingNumber method to use sum formula for optimization.

--- Missing Number.py
+++ Missing Number.py
@@ -1,5 +1,15 @@
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         for i in range(len(nums)+1):
+#             if i not in nums:
+#                 return i
+
+# optimized 
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        n = len(nums)
+        # taking the sum of expected outcome numbers form 0 - n and subtracting what's actually found inside an array
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
