23525965fe0892483f61232f4cf51bc0b9dc6790
Nardos Tsige <tsigeferejanardos@gmail.com>
2026-03-19 04:08:38 +0300

    Update Number of Segments in a String.py

--- Number of Segments in a String.py
+++ Number of Segments in a String.py
@@ -1,3 +1,3 @@
 class Solution:
     def countSegments(self, s: str) -> int:
-        return len(s.split())
+        return len(s.split())#split with no argument inside it, returns element words in a list
