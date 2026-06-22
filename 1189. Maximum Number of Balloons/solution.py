3bf7c931613d806aa53724d4ec81cbe8825ffee5
Nardos Tsige <tsigeferejanardos@gmail.com>
2026-06-22 12:18:00 +0300

    Implement maxNumberOfBalloons method in Solution class

---
+++ 1189. Maximum Number of Balloons/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = Counter(text)
+        return min(
+            count['b'],
+            count['a'],
+            count['l'] // 2,
+            count['o'] // 2,
+            count['n']
+        )
