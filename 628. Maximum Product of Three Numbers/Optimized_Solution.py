b354e314771218bd0ee1fa6bfdbb159392cd1a04
Nardos Tsige <tsigeferejanardos@gmail.com>
2026-08-01 14:22:39 +0300

    Add optimized solution for maximum product of three numbers

---
+++ 628. Maximum Product of Three Numbers/Optimized_Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        #case 1: three largest numbers
+        case1 = nums[-1] * nums[-2] * nums[-3]
+        #case 2: two smallest(most negative) and the largest
+        case2 = nums[0] * nums[1] * nums[-1]
+        return max(case1, case2)
