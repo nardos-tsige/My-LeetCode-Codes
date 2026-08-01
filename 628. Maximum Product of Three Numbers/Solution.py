29acb8d5378269e606a56bc2355526e4d002563e
Nardos Tsige <tsigeferejanardos@gmail.com>
2026-08-01 14:18:49 +0300

    Implement brute force solution for maximum product

---
+++ 628. Maximum Product of Three Numbers/Solution.py
@@ -0,0 +1,11 @@
+def maximumProduct_bruteforce(nums):
+    n = len(nums)
+    max_product = float('-inf')
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                product = nums[i] * nums[j] * nums[k]
+                max_product = max(max_product, product)
+    
+    return max_product
